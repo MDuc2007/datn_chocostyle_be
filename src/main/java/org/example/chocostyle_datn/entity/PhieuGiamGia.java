3cc84503a4949d02417bfb9dc591343c093ae6be
MinhTu <ngyuendangminhtu@gmail.com>
2026-01-18 21:02:26 +0700

    muá»up code lại

--- src/main/java/org/example/chocostyle_datn/entity/PhieuGiamGia.java
+++ src/main/java/org/example/chocostyle_datn/entity/PhieuGiamGia.java
@@ -1,38 +1,44 @@
 package org.example.chocostyle_datn.entity;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.hibernate.annotations.Nationalized;
 
 import java.math.BigDecimal;
-import java.time.Instant;
 import java.time.LocalDate;
 
 @Getter
 @Setter
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
 @Table(name = "phieu_giam_gia")
 public class PhieuGiamGia {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id_pgg", nullable = false)
+    @Column(name = "id_pgg")
     private Integer id;
 
-    @Size(max = 50)
-    @NotNull
     @Column(name = "ma_pgg", nullable = false, length = 50)
     private String maPgg;
 
-    @Size(max = 50)
-    @NotNull
-    @Nationalized
+    @Column(name = "ten_pgg", nullable = false, length = 50)
+    private String tenPgg;
+
+    @Column(name = "kieu_ap_dung", nullable = false, length = 20)
+    private String kieuApDung;
+
     @Column(name = "loai_giam", nullable = false, length = 50)
     private String loaiGiam;
 
-    @NotNull
     @Column(name = "gia_tri", nullable = false, precision = 18, scale = 2)
     private BigDecimal giaTri;
 
@@ -42,24 +48,18 @@ public class PhieuGiamGia {
     @Column(name = "dieu_kien_don_hang", precision = 18, scale = 2)
     private BigDecimal dieuKienDonHang;
 
-    @NotNull
     @Column(name = "ngay_bat_dau", nullable = false)
     private LocalDate ngayBatDau;
 
-    @NotNull
     @Column(name = "ngay_ket_thuc", nullable = false)
     private LocalDate ngayKetThuc;
 
-    @NotNull
     @Column(name = "so_luong", nullable = false)
     private Integer soLuong;
 
-    @NotNull
     @Column(name = "so_luong_da_dung", nullable = false)
     private Integer soLuongDaDung;
 
-    @NotNull
     @Column(name = "trang_thai", nullable = false)
     private Integer trangThai;
-
 }
---
+++ src/main/java/org/example/chocostyle_datn/model/response/PhieuGiamGiaResponse.java
@@ -0,0 +1,31 @@
+package org.example.chocostyle_datn.model.Response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class PhieuGiamGiaResponse {
+    private Integer id;
+    private String maPgg;
+    private String tenPgg;
+    private String kieuApDung;
+    private String loaiGiam;
+    private BigDecimal giaTri;
+    private BigDecimal giaTriToiDa;
+    private BigDecimal dieuKienDonHang;
+    private LocalDate ngayBatDau;
+    private LocalDate ngayKetThuc;
+    private Integer soLuong;
+    private Integer soLuongDaDung;
+    private Integer trangThai;
+}
+
