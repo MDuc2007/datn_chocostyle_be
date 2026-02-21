2a1bc2445573b0728c987f72faeb425f6e37fff4
Admin <hungbe0of@gmail.com>
2026-02-21 16:01:09 +0700

    Nội duquyet ngu

---
+++ src/main/java/org/example/chocostyle_datn/repository/ChamCongRepository.java
@@ -0,0 +1,14 @@
+package org.example.chocostyle_datn.repository;
+
+import org.example.chocostyle_datn.entity.ChamCong;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
+import java.util.Optional;
+
+@Repository
+public interface ChamCongRepository extends JpaRepository<ChamCong, Integer> {
+
+    Optional<ChamCong> findByNhanVien_IdAndNgay(Integer idNv, LocalDate ngay);
+}
